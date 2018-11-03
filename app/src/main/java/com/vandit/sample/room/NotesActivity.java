86586e591a1e2ed3a5bc6243a0416abb3e3b8270
Patel, Vanditkumar (ELS-CON) <v.patel@elsevier.com>
2018-11-03 19:55:44 -0400

    Add data binding instead of butter knife

--- app/src/main/java/com/vandit/sample/room/NotesActivity.java
+++ app/src/main/java/com/vandit/sample/room/NotesActivity.java
@@ -5,23 +5,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.vandit.sample.room.databinding.ActivityNotesBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 
 public class NotesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ActivityNotesBinding activityNotesBinding = DataBindingUtil.setContentView(this, R.layout.activity_notes);
+        setSupportActionBar(activityNotesBinding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        activityNotesBinding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
